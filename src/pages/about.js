451e1b6f1721db96375b3e408a5f4c823386570c
othwsav <54482114+othwsav@users.noreply.github.com>
2021-07-03 17:54:04 +0100

    added some content to about and home pages + added images folder to assets folder + installed gatsby-plugin-image in the project

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,25 @@
+import { Link } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
 import React from 'react'
 import Layout from '../components/Layout'
 
 const About = () => {
     return (
         <Layout>
-            <main>
-                <h1>
-                    Hello from About page
-                </h1>
+            <main className="page">
+                <section className="about-page">
+                    <article>
+                        <h2>Lorem ipsum dolor sit amet</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe debitis distinctio ipsam velit commodi?</p>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <Link to="/contact" className="btn">contact</Link>
+                    </article>
+                    <StaticImage
+                    src="../assets/images/about.jpeg"
+                    alt="person pouring salt in bowl"
+                    className="about-img"
+                    placeholder="blurred"/>
+                </section>
             </main>
         </Layout>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,27 @@ import React from 'react'
 import Layout from '../components/Layout'
 import '../assets/css/style.css'
 import 'normalize.css'
+import { StaticImage } from 'gatsby-plugin-image'
 
 
 export default function Home () {
 	return (
 		<Layout>
-			<main>
-				<h1>first gatsby projects</h1>
+			<main className="page">
+				<header className="hero">
+					<StaticImage
+						src="../assets/images/main.jpeg"
+						alt="eggs and flour"
+						className="hero-img"
+						placeholder="tracedSVG"
+						layout="fullWidth" />
+					<div className="hero-container">
+						<div className="hero-text">
+							<h1>Simple recipes</h1>
+							<h4>no fluff, just recipes</h4>
+						</div>
+					</div>
+				</header>
 			</main>
 		</Layout>
 	)
